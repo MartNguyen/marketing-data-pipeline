e88c8d4a3a9282711076e7ae5c4fbea768de27c4
Nguyễn Vũ Minh Quân <163006647+MartNguyen@users.noreply.github.com>
2026-05-25 17:10:44 +0700

    Update backfill.py

--- backfill.py
+++ backfill.py
@@ -103,7 +103,7 @@ def run_pipeline():
     os.environ["DESTINATION__BIGQUERY__CREDENTIALS__CLIENT_EMAIL"] = os.environ.get("GCP_CLIENT_EMAIL")
     os.environ["DESTINATION__BIGQUERY__CREDENTIALS__PRIVATE_KEY"] = os.environ.get("GCP_PRIVATE_KEY", "").replace("\\n", "\n")
 
-    pipeline = dlt.pipeline(pipeline_name="meta_ultimate_v15_4", destination="bigquery", dataset_name="fb_ads_master_v4")
+    pipeline = dlt.pipeline(pipeline_name="meta_ultimate_v15_4_refresh", destination="bigquery", dataset_name="fb_ads_master_v4")
     token = os.environ.get("FB_ACCESS_TOKEN")
 
     account_groups = {
